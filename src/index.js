a1c864a0bc8062b2c55d2ebd210c938ec14bf0e9
SerhioAndros <androsiuksv@gmail.com>
2021-05-21 13:14:46 +0300

    Added functionality (pages, render etc)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import './sass/main.scss';
 import { fetchImages } from './js/apiService.js';
-const formNode = document.querySelector('#search-form');
-const formInput = formNode.firstElementChild;
+import makeGalMarkup from './templates/card.hbs';
+const nodes = {
+  formNode: document.querySelector('#search-form'),
+  formInput: document.querySelector('input'),
+  galleryNode: document.querySelector('.gallery'),
+  addImgBtn: document.querySelector('.add-imgs-btn'),
+};
+// const formNode = document.querySelector('#search-form');
+// const formInput = formNode.firstElementChild;
+// const galleryNode = document.querySelector('.gallery');
+let requestString = '';
+let currPage = 1;
 
-formNode.addEventListener('click', searchImgFunction);
+nodes.formNode.addEventListener('click', searchImgFunction);
 
 function searchImgFunction(e) {
   if (e.target.tagName !== 'BUTTON') return false;
-  //   console.log(fetchImages(formInput.value));
-  console.log(typeof formInput.value.trim());
+  requestString = stringSpaceEraze(nodes.formInput.value.trim());
+  getImgs(requestString, currPage);
 }
-// function stringSpaceEraze(string) {
-//   // let newString = "",
-// }
+
+function getImgs(request, page) {
+  fetchImages(request, page).then(data => (nodes.galleryNode.innerHTML = makeGalMarkup(data)));
+  currPage += 1;
+}
+
+function stringSpaceEraze(string) {
+  return string.split(' ').join('%20');
+}
+
+nodes.addImgBtn.addEventListener('click', addImages);
+
+function addImages(e) {
+  fetchImages(requestString, currPage).then(data =>
+    nodes.galleryNode.insertAdjacentHTML('beforeend', makeGalMarkup(data)),
+  );
+  currPage += 1;
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=';
 const apiKey = '21715456-94146d2128778e129cf5897fe';
-let page = 1;
+// let page = 2;
 
-const fetchImages = request =>
+const fetchImages = (request, page) =>
   fetch(`${BASE_URL}${request}&page=${page}&per_page=12&key=${apiKey}`).then(res => {
     if (res.status === 404) {
       throw new Error(`There are no such IMG in data-base`);
